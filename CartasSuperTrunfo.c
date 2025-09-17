a540609239e2495ef8afd94281884e83105b7f4c
PABLO SOUSA <pablosousaoliver@gmail.com>
2025-09-17 13:22:49 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-    int main() {
-    
+    int main(){
+        int carta = 1;
+        float altura = 1.75;
+        char opcoes = 'S';
+        char nome[20] = "sergio";
+
+        printf("A idade do %s é: %\n", nome, idade);
+        printf("A altura é: %e\n", altura);
+        printf("A opcao é: %c\n, opcao");    
 
     return 0;
 }
